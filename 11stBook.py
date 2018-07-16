0883f2877037648950c8f498b0bfb3b002360b1b
huewilliams <37013834+huewilliams@users.noreply.github.com>
2018-07-16 23:39:04 +0900

    11번가도서 베스트셀러 크롤링

---
+++ 11stBook.py
@@ -0,0 +1,26 @@
+# parser.py
+import requests
+from bs4 import BeautifulSoup
+import os
+
+# python 파일의 위치
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+req = requests.get('http://books.11st.co.kr/booksmall/BooksAction.tmall?ID=BOOKS&ctgrNo=63548')
+
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+my_titles = soup.select(
+        '#layBody > div.books_subWrap > div > ul > li > div > div > div.pup_title > a'
+    )
+data = {}
+
+for title in my_titles:
+    data[title.text] = title.get('text')
+print(data)
+i = 0
+for k in data.keys():
+    i=i+1
+    print('['+str(i)+']  ' + k[4:])
+    if (i>=20):
+        break
