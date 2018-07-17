639eca59bcfb656caccfcae4c447b0e15b9285ab
huewilliams <37013834+huewilliams@users.noreply.github.com>
2018-07-17 23:43:49 +0900

    소나타팀프로젝트-베스트셀러종합컴바인(GUI+kyobo.py기능추가).ver2
    기존의 GUI만 구현되어 있던 BooksCombine.py에 교보문고 버튼을 눌렀을 때 kyobo.py의 기능인
    교보문고 베스트셀러 리스트가 버튼으로 나타나고
    버튼을 클릭하면 해당 도서의 상품페이지로 이동한다.

--- BooksCombine.py
+++ BooksCombine.py
@@ -1,7 +1,19 @@
 # GUI import
 from tkinter import *
 
+# 크롤링 import
+import requests
+from bs4 import BeautifulSoup
+import os
+
+# webBrowser import
+import webbrowser
+
+#메인 GUI (인터넷 서점 사이트 선택 버튼)
 master = Tk()
+
+decide = 0
+# decide : 도서 구매 사이트 결정 변수 0:default, 1:교보, 2:11번가, 3:리디북스
 
 
 def best():
@@ -10,7 +22,59 @@ def best():
 
 def kyobo():
     print('교보문고 베스트셀러 목록')
+    # python 파일의 위치
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+    req = requests.get('http://www.kyobobook.co.kr/bestseller/bestSellerMain.laf?orderClick=d79')
+
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # 베스트 셀러 URL 크롤링
+    my_titles = soup.select(
+        '#main_contents > ul > li > div.cover > a'
+    )
+    link = {}
+
+    for title in my_titles:
+        link[title.text] = title.get('href')
+    i = 0
+    for k in link.values():
+        i = i + 1
+        # print('['+str(i)+']  '+k)
+        print(f'[{i}] {k}')
+
+    # 베스트 셀러 URL 크롤링 완료
+
+    # 베스트 셀러 데이터 크롤링
+    my_titles = soup.select(
+        '#main_contents > ul > li > div.detail > div.title > a > strong'
+    )
+    data = {}
+
+    for title in my_titles:
+        data[title.text] = title.get('text')
+
+    # 베스트 셀러 크롤링 완료
+
+    # 함수 정의
+    def practice(i):
+        # 래퍼 함수
+        def inner():
+            print('sucess')
+            x = list(link.values())
+            print(i)
+            url = str(x[i - 1])
+            webbrowser.open(url)
+
+        return inner
+
+    i = 0
+    for k in data.keys():
+        i = i + 1
+        print('[' + str(i) + ']  ' + k)
+        a = Button(master, text='[' + str(i) + ']  ' + k, command=practice(i))
+        a.grid(row=i, column=10)
 
 def book11st():
     print('11번가 베스트셀러 목록')
@@ -31,4 +95,6 @@ a.grid(row=1, column=1)
 
 a = Button(master, text='리디북스', command=ridi)
 a.grid(row=1, column=2)
+#메인 GUI 끝
+
 mainloop()
