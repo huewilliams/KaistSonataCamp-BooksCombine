dd46dfefd35c6ac8300798660f8fbc805bfd605f
huewilliams <37013834+huewilliams@users.noreply.github.com>
2018-07-18 07:33:39 +0900

    소나타팀프로젝트-베스트셀러종합컴바인(ver2+11stCombine기능추가).ver3
    ver2에서는 교보문고리스트만 선택했을 때 베스트셀러 리스트가 나타나고 버튼을 클릭하면 해당 도서의 상품 페이지로 이동했다.
    ver3에서는 11stCombine의 기능을 추가하여 11번가도서 버튼을 누르면 11번가 베스트셀러 리스트가 나타나고 버튼을 클릭하면 해당 도서의 상품페이지로 이동하는 것을 구현했다.
    교보를 선택한 후 11번가를 누르면 기존의 리스트 위를 덮으며 새로은 버튼리스트가 나타난다.

--- BooksCombine.py
+++ BooksCombine.py
@@ -22,8 +22,6 @@ def best():
 
 def kyobo():
     print('교보문고 베스트셀러 목록')
-    # python 파일의 위치
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     req = requests.get('http://www.kyobobook.co.kr/bestseller/bestSellerMain.laf?orderClick=d79')
 
@@ -78,7 +76,60 @@ def kyobo():
 
 def book11st():
     print('11번가 베스트셀러 목록')
+    req = requests.get('http://books.11st.co.kr/booksmall/BooksAction.tmall?ID=BOOKS&ctgrNo=63548')
 
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # 베스트 셀러 URL 크롤링
+    my_titles = soup.select(
+        '#layBody > div.books_subWrap > div > ul > li > div > div > div.pup_title > a'
+    )
+    link = {}
+
+    for title in my_titles:
+        link[title.text] = title.get('href')
+    i = 0
+    for k in link.values():
+        i = i + 1
+        # print('['+str(i)+']  '+k)
+        print(f'[{i}] {k}')
+        if (i >= 20):
+            break
+
+    # 베스트 셀러 URL 크롤링 완료
+
+    # 베스트 셀러 데이터 크롤링
+    my_titles = soup.select(
+        '#layBody > div.books_subWrap > div > ul > li > div > div > div.pup_title > a'
+    )
+    data = {}
+
+    for title in my_titles:
+        data[title.text] = title.get('text')
+
+    # 베스트 셀러 크롤링 완료
+
+    # 함수 정의
+    def practice(i):
+        # 래퍼 함수
+        def inner():
+            print('sucess')
+            x = list(link.values())
+            print(i)
+            url = str(x[i - 1])
+            webbrowser.open(url)
+
+        return inner
+
+    i = 0
+    for k in data.keys():
+        i = i + 1
+        print('[' + str(i) + ']  ' + k)
+        a = Button(master, text='[' + str(i) + ']  ' + k, command=practice(i))
+        a.grid(row=i, column=10)
+        if (i >= 20):
+            break
 
 def ridi():
     print('리디북스 베스트셀러 목록')
