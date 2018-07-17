4725357555284a57d29d897ab9f631ac187ab408
huewilliams <37013834+huewilliams@users.noreply.github.com>
2018-07-18 08:11:42 +0900

    소나타팀프로젝트-베스트셀러종합컴바인(ver3+ridiCombine기능추가).ver4
    기존의 ver3에서는 교보문고와 11번가 도서의 베스트리스트목록이 가능했다.
    이번 업데이트에서는 RidiCombine.py의 기능을 추가했다.

--- BooksCombine.py
+++ BooksCombine.py
@@ -74,6 +74,7 @@ def kyobo():
         a = Button(master, text='[' + str(i) + ']  ' + k, command=practice(i))
         a.grid(row=i, column=10)
 
+
 def book11st():
     print('11번가 베스트셀러 목록')
     req = requests.get('http://books.11st.co.kr/booksmall/BooksAction.tmall?ID=BOOKS&ctgrNo=63548')
@@ -131,9 +132,65 @@ def book11st():
         if (i >= 20):
             break
 
+
 def ridi():
     print('리디북스 베스트셀러 목록')
 
+    req = requests.get('https://ridibooks.com/bestsellers/general?order=monthly')
+
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # 베스트 셀러 URL 크롤링
+    my_titles = soup.select(
+        '#page_best > div.book_macro_wrapper.js_book_macro_wrapper > div > div.book_metadata_wrapper > h3 > a'
+    )
+    link = {}
+
+    for title in my_titles:
+        link[title.text] = title.get('href')
+    i = 0
+    for k in link.values():
+        i = i + 1
+        # print('['+str(i)+']  '+k)
+        print(f'[{i}] {k}')
+        if (i >= 20):
+            break
+
+    # 베스트 셀러 URL 크롤링 완료
+
+    # 베스트 셀러 데이터 크롤링
+    my_titles = soup.select(
+        '#page_best > div.book_macro_wrapper.js_book_macro_wrapper > div > div.book_metadata_wrapper > h3 > a > span'
+    )
+    data = {}
+
+    for title in my_titles:
+        data[title.text] = title.get('text')
+
+    # 베스트 셀러 크롤링 완료
+
+    # 함수 정의
+    def practice(i):
+        # 래퍼 함수
+        def inner():
+            print('sucess')
+            x = list(link.values())
+            print(i)
+            url = 'https://ridibooks.com' + str(x[i - 1])
+            webbrowser.open(url)
+
+        return inner
+
+    i = 0
+    for k in data.keys():
+        i = i + 1
+        print('[' + str(i) + ']  ' + k)
+        tmp = k.strip()
+        a = Button(master, text='[' + str(i) + ']  ' + tmp, command=practice(i))
+        a.grid(row=i, column=10)
+        if (i >= 20):
+            break
 
 a = Button(master, text='베스트셀러 리스트', command=best)
 a.grid(row=0, column=1)
