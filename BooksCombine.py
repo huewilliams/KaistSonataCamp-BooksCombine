380883fa39272f3aa2f5f31c06948567e583c0e5
huewilliams <37013834+huewilliams@users.noreply.github.com>
2018-07-18 17:18:09 +0900

    소나타팀프로젝트-베스트셀러종합컴바인(ver5+리디북스디버깅+tkTitle변경).ver6
    웹 크롤링과정에서 리디북스의 베스트셀러의 1위가 예상치 못한 데이터가 나왔고 기존의 1위는 2위로 나왔다. 그래서 21개의 데이터를 받고 
    0번 데이터를 삭제했다. => 리디북스 크롤링 오류 해결
    tkTitle이 모두 TK로 명명되어있어 어떤 데이터인지 알아보기 힘들었다. master.title 을 변경하는 것으로 팝업창의 title까지 변경하여 구분하기 쉽게 되었다,

--- BooksCombine.py
+++ BooksCombine.py
@@ -4,25 +4,31 @@ from tkinter import *
 # 크롤링 import
 import requests
 from bs4 import BeautifulSoup
-import os
-
 # webBrowser import
 import webbrowser
 
 #메인 GUI (인터넷 서점 사이트 선택 버튼)
 master = Tk()
-
+master.title("BestSellerList")
 decide = 0
 # decide : 도서 구매 사이트 결정 변수 0:default, 1:교보, 2:11번가, 3:리디북스
 
-
 def best():
     print('베스트셀러 리스트입니다')
+    a = Button(master, text='교보문고', command=kyobo)
+    a.grid(row=1, column=0)
+
+    a = Button(master, text='11번가 도서', command=book11st)
+    a.grid(row=1, column=1)
+
+    a = Button(master, text='리디북스', command=ridi)
+    a.grid(row=1, column=2)
 
 
 def kyobo():
     print('교보문고 베스트셀러 목록')
     mKyobo = Tk()
+    mKyobo.title("교보문고")
 
     req = requests.get('http://www.kyobobook.co.kr/bestseller/bestSellerMain.laf?orderClick=d79')
 
@@ -79,6 +85,7 @@ def kyobo():
 def book11st():
     print('11번가 베스트셀러 목록')
     m11st = Tk()
+    m11st.title("11번가 도서")
 
     req = requests.get('http://books.11st.co.kr/booksmall/BooksAction.tmall?ID=BOOKS&ctgrNo=63548')
 
@@ -112,6 +119,7 @@ def book11st():
     for title in my_titles:
         data[title.text] = title.get('text')
 
+
     # 베스트 셀러 크롤링 완료
 
     # 함수 정의
@@ -128,18 +136,35 @@ def book11st():
 
     i = 0
     for k in data.keys():
-        i = i + 1
         print('[' + str(i) + ']  ' + k)
         a = Button(m11st, text='[' + str(i) + ']  ' + k, command=practice(i))
         a.grid(row=i, column=10)
+        i = i + 1
         if (i >= 20):
             break
+    #가격 정보 크롤링
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    my_titles = soup.select(
+        '#layBody > div.books_subWrap > div.thumbnail_list.thumb_w780 > ul > li > div > div > div.pub_priceW > strong'
+    )
+    data = {}
 
-
+    for title in my_titles:
+        data[title.text] = title.get('text')
+    i = 0
+    for k in data.keys():
+        i = i + 1
+        # print('['+str(i)+']  '+k)
+        print(f'[{i}] {k}')
+        if (i >= 20):
+            break
+    #가격 정보 크롤링 완료
 def ridi():
     print('리디북스 베스트셀러 목록')
 
     mRidi = Tk()
+    mRidi.title("리디북스")
     req = requests.get('https://ridibooks.com/bestsellers/general?order=monthly')
 
     html = req.text
@@ -153,13 +178,15 @@ def ridi():
 
     for title in my_titles:
         link[title.text] = title.get('href')
+
     i = 0
     for k in link.values():
+        print(f'[{i}] {k}')
         i = i + 1
         # print('['+str(i)+']  '+k)
-        print(f'[{i}] {k}')
-        if (i >= 20):
+        if (i >= 21):
             break
+
 
     # 베스트 셀러 URL 크롤링 완료
 
@@ -181,32 +208,27 @@ def ridi():
             print('sucess')
             x = list(link.values())
             print(i)
-            url = 'https://ridibooks.com' + str(x[i - 1])
+            url = 'https://ridibooks.com' + str(x[i])
             webbrowser.open(url)
 
         return inner
 
     i = 0
     for k in data.keys():
-        i = i + 1
+
         print('[' + str(i) + ']  ' + k)
         tmp = k.strip()
-        a = Button(mRidi, text='[' + str(i) + ']  ' + tmp, command=practice(i))
-        a.grid(row=i, column=10)
-        if (i >= 20):
+        if (i == 0):
+            print('예외 처리')
+        else :
+            a = Button(mRidi, text='[' + str(i) + ']  ' + tmp, command=practice(i))
+            a.grid(row=i, column=10)
+        i = i + 1
+        if (i >= 21):
             break
+
 
 a = Button(master, text='베스트셀러 리스트', command=best)
 a.grid(row=0, column=1)
-
-a = Button(master, text='교보문고', command=kyobo)
-a.grid(row=1, column=0)
-
-a = Button(master, text='11번가 도서', command=book11st)
-a.grid(row=1, column=1)
-
-a = Button(master, text='리디북스', command=ridi)
-a.grid(row=1, column=2)
-#메인 GUI 끝
 
 mainloop()
