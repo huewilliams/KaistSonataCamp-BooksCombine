7389713221f711b0516ee9fcc41599d63dd4f6a5
huewilliams <37013834+huewilliams@users.noreply.github.com>
2018-07-21 20:42:23 +0900

    소나타팀프로젝트-베스트셀러종합컴바인.ver8(대회 시연 버전)
    리디북스에서 책의 표지이미지를 볼 수 있는 크롤링에 성공하였다.
    이 프로그램으로 카이스트 본원에서 발표하였는데
    2등을 하여 장려상을 수여받았다.

--- BooksCombine.py
+++ BooksCombine.py
@@ -1,22 +1,24 @@
 # GUI import
 from tkinter import *
 
+import shutil
+import os
+
 # 크롤링 import
 import requests
 from bs4 import BeautifulSoup
 # webBrowser import
 import webbrowser
 
-#메인 GUI (인터넷 서점 사이트 선택 버튼)
+from PIL import ImageTk
+
+# 메인 GUI (인터넷 서점 사이트 선택 버튼)
 master = Tk()
 master.title("BestSellerList")
 decide = 0
 # decide : 도서 구매 사이트 결정 변수 0:default, 1:교보, 2:11번가, 3:리디북스
-canvas_width = 100
-canvas_height = 100
-w = Canvas(master,
-           width = canvas_width,
-           height = canvas_height)
+
+
 def best():
     print('베스트셀러 리스트입니다')
     a = Button(master, text='교보문고', command=kyobo)
@@ -82,7 +84,8 @@ def kyobo():
         i = i + 1
         print('[' + str(i) + ']  ' + k)
         a = Button(mKyobo, text='[' + str(i) + ']  ' + k, command=practice(i))
-        a.grid(row=i, column=10)
+        a.config(width=75, height=1)
+        a.pack(side=TOP)
 
 
 def book11st():
@@ -132,6 +135,37 @@ def book11st():
             break
     # 가격 정보 크롤링 완료
 
+    # 베스트셀러 이미지 크롤링
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    my_titles = soup.select(
+        '#layBody > div > div > ul > li > div > div.pub_photo > a > span > img'
+    )
+    directory = os.path.dirname('./img/')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    data = []
+    source = []
+    print(my_titles)
+    for title in my_titles:
+        data.append(title.get('alt'))
+        source.append(title.get('src'))
+    print(data)
+    print(source)
+    i = 0
+    for src in source:
+        img_src = source[i]
+        img_name = str(i)
+        i = i + 1
+        r = requests.get(img_src, stream=True, headers={'User-agent': 'Mozilla/5.0'})
+        if r.status_code == 200:
+            with open('./img/' + img_name + '.jpeg', 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+        if i == 20:
+            break
+    # 베스트셀러 이미지 크롤링 완료
+
     # 베스트 셀러 데이터 크롤링
     my_titles = soup.select(
         '#layBody > div.books_subWrap > div > ul > li > div > div > div.pup_title > a'
@@ -149,21 +183,39 @@ def book11st():
         # 래퍼 함수
         def inner():
             def goUrl():
-                url = str(x[i - 1])
+                url = str(x[i-1])
                 webbrowser.open(url)
             print('sucess')
             x = list(link.values())
             p = list(price.keys())
             k = list(data.keys())
-
+            print(p)
+            def showImage():
+                print(source[i-1])
+                SI = Toplevel()
+                canvas_width = 150
+                canvas_height = 200
+                w = Canvas(SI,
+                           width=canvas_width,
+                           height=canvas_height)
+                w.pack()
+                img = ImageTk.PhotoImage(file="./img/"+str(i-1)+'.jpeg')
+                print("./img/"+str(i-1)+'.jpeg')
+                w.create_image(0, 0, anchor=NW, image=img)  # 이미지를 마진을 설정
+                SI.mainloop()
             i11st = Tk()
             i11st.title('책 정보 : '+k[i-1])
-            ia = Button(i11st, text='가격 : '+p[i-1])
-            ia.grid(row=0,column=0)
+            ia = Button(i11st, text='가격 : '+p[i-2])
+            ia.config(width=30, height=2)
+            ia.pack(side=TOP)
             ia = Button(i11st, text='사러가기',command=goUrl)
-            ia.grid(row=1,column=0)
+            ia.config(width=30, height=2)
+            ia.pack(side=TOP)
+            ia = Button(i11st, text='책 이미지보기', command=showImage)
+            ia.config(width=30, height=2)
+            ia.pack(side=TOP)
             print(i)
-            print(p[i - 1])
+            print(p[i])
 
         return inner
 
@@ -172,7 +224,8 @@ def book11st():
         i = i + 1
         print('[' + str(i) + ']  ' + k)
         a = Button(m11st, text='[' + str(i) + ']  ' + k, command=practice(i))
-        a.grid(row=i, column=10)
+        a.config(width=100, height=1)
+        a.pack(side=TOP)
         if (i >= 20):
             break
 
@@ -206,6 +259,26 @@ def ridi():
 
     # 베스트 셀러 URL 크롤링 완료
 
+    #가격 정보 크롤링
+
+    my_titles = soup.select(
+        '#page_best > div > div > div.book_metadata_wrapper > div > p > span.price > span'
+    )
+
+    price = {}
+
+    for title in my_titles:
+        price[title.text] = title.get('text')
+    i = 0
+    print(price)
+    for p in price.keys():
+        i = i + 1
+        tmp = p.strip()
+        print('[' + str(i) + ']   ' + tmp + '원')
+        if (i >= 20):
+            break
+    #가격 정보 크롤링 완료
+
     # 베스트 셀러 데이터 크롤링
     my_titles = soup.select(
         '#page_best > div.book_macro_wrapper.js_book_macro_wrapper > div > div.book_metadata_wrapper > h3 > a > span'
@@ -222,23 +295,38 @@ def ridi():
         # 래퍼 함수
         def inner():
             print('sucess')
+            def goUrl():
+                url = 'https://ridibooks.com' + str(x[i])
+                webbrowser.open(url)
             x = list(link.values())
+            p = list(price.keys())
+            k = list(data.keys())
+
+            iRidi = Tk()
+            iRidi.title('책 정보 : ' + k[i].strip())
+
+            ia = Button(iRidi, text='가격 : ' + p[i])
+            ia.config(width=30, height=1)
+            ia.pack(side=TOP)
+            ia = Button(iRidi, text='사러가기', command=goUrl)
+            ia.config(width=30, height=1)
+            ia.pack(side=TOP)
             print(i)
-            url = 'https://ridibooks.com' + str(x[i])
-            webbrowser.open(url)
+            print(p[i])
 
         return inner
 
     i = 0
     for k in data.keys():
-
         print('[' + str(i) + ']  ' + k)
         tmp = k.strip()
         if (i == 0):
             print('예외 처리')
         else :
-            a = Button(mRidi, text='[' + str(i) + ']  ' + tmp, command=practice(i))
-            a.grid(row=i, column=10)
+            iText = '[' + str(i) + ']  ' + tmp
+            a = Button(mRidi, text=iText, command=practice(i))
+            a.config(width=75, height=1)
+            a.pack(side=TOP)
         i = i + 1
         if (i >= 21):
             break
@@ -246,5 +334,6 @@ def ridi():
 
 a = Button(master, text='베스트셀러 리스트', command=best)
 a.grid(row=0, column=1)
+
 
 mainloop()
